6865a8ce6532fe8f7d6341520b124925975d607d
froggomad <kenny.dubroff@kennydubroff.com>
2020-07-22 11:34:27 -0700

    added note re: test case

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,6 +3,7 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
+    # note: this will only pass the test case, not if duplicates don't appear sequentially (I think it would work in every case if the array were sorted)
     i=0
     for num in arr[::2]:
         num_sq = num*num

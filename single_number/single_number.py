11616c787dce2856ef14ee5da6fdd6d63c39ad72
froggomad <kenny.dubroff@kennydubroff.com>
2020-07-23 14:49:25 -0700

    refactored

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,15 +4,22 @@ Returns: an integer
 '''
 def single_number(arr):
     # note: this will only pass the test case, not if duplicates don't appear sequentially (I think it would work in every case if the array were sorted)
-    i=0
-    for num in arr[::2]:
-        num_sq = num*num
-        num_next = num * arr[i+1]
-        nums_match = num_sq == num_next
-        i+=2
+    # i=0
+    # for num in arr[::2]:
+    #     num_sq = num*num
+    #     num_next = num * arr[i+1]
+    #     nums_match = num_sq == num_next
+    #     i+=2    
+    #     if not nums_match:
+    #       return num
+    s = set()
+    for x in arr:
+        if x in s:
+            s.remove(x)
+        else:
+            s.add(x)
+    return list(s)[0]
 
-        if not nums_match:
-            return num
 
 
 if __name__ == '__main__':

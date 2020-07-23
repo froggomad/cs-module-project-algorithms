89ad4b94fa9666bea21866b7278ec86e75e915b1
froggomad <kenny.dubroff@kennydubroff.com>
2020-07-23 14:48:56 -0700

    refactored

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,23 +4,30 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     i = 0
-    z = 0
-    
-    while i is not len(arr):        
+    #track whether we've deleted the first zero
+    deleted = False
+    #track its index in the array
+    first_zero_i = -1
+    #when we hit the first deleted zero, everything after is also zero
+    while i is not first_zero_i and i is not len(arr)-1:
+        print(i)        
         if arr[i] == 0:
             arr.__delitem__(i)
             print(f"deleting at: {i}")
-            # decrement current index            
-            i-=1
-            # increase zero count
-            z+=1  
-        # increase current index         
-        i+=1
+            arr.append(0)
+            if deleted == False:
+                #set the first deleted zero's index
+                first_zero_i = len(arr)-1
+                print(f"deleted first zero and inserted at {len(arr)-1}")
+                deleted = True
+            else:
+                #move it backward to account for deleted zeroes
+                first_zero_i -= 1
+        else:
+            # increase current index         
+            i+=1
+        
         print(arr)
-    #append zeroes
-    for _ in range(z):
-        arr.append(0)
-        print("i")
     return arr
 
 
